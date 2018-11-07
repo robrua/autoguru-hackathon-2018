8ea7defcf08f670a5cb24d20b664b252c138569e
Rob Rua <robertrua@gmail.com>
2018-11-07 16:41:53 -0500

    shell project for question answering

---
+++ question-answering/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    "gensim"
+]
+
+setup(
+    name="questionanswering",
+    version="0.0.1",
+    author="The AutoGuru Team",
+    author_email="",
+    url="https://github.com/robrua/autoguru",
+    description="Taking our jobs one Guru at a time",
+    license="MIT",
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=install_requires,
+    include_package_data=True
+)
