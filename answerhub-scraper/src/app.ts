0dd139ac124a5c9c58137d664f1226298b3d5625
Matthew Oslan <derpthemeus@gmail.com>
2018-11-08 10:01:43 -0800

    Add AnswerHub comment downloader

--- answerhub-scraper/src/app.ts
+++ answerhub-scraper/src/app.ts
@@ -1,7 +1,8 @@
 import AnswerHub from "./AnswerHub";
 import fs = require("fs");
 
-const LAST_PAGE_FILE = `${__dirname}/../data/last_question.txt`;
+const LAST_QUESTION_PAGE_FILE = `${__dirname}/../data/last_question_page.txt`;
+const LAST_COMMENT_PAGE_FILE = `${__dirname}/../data/last_comment_page.txt`;
 const CONFIG_FILE = `${__dirname}/../config.json`;
 const config = JSON.parse(fs.readFileSync(CONFIG_FILE).toString());
 
@@ -13,21 +14,35 @@ const answerHub = new AnswerHub(
 
 const downloadQuestions = async () => {
 	let lastPage: number = 0;
-	if (fs.existsSync(LAST_PAGE_FILE)) {
-		const contents: string = fs.readFileSync(LAST_PAGE_FILE).toString();
+	if (fs.existsSync(LAST_QUESTION_PAGE_FILE)) {
+		const contents: string = fs.readFileSync(LAST_QUESTION_PAGE_FILE).toString();
 		lastPage = +contents;
 	}
 	// Determine how many pages of questions there are because AnswerHub doesn't let you sort by oldest questions first
 	const pageCount = (await answerHub.getQuestions(0, "newest")).pageCount;
 	for (let page = pageCount - lastPage; page >= 0; page--) {
-		await downloadPage(page);
-		fs.writeFileSync(LAST_PAGE_FILE, page);
+		await downloadQuestionPage(page);
+		fs.writeFileSync(LAST_QUESTION_PAGE_FILE, page);
+	}
+};
+
+const downloadComments = async () => {
+	let lastPage: number = 0;
+	if (fs.existsSync(LAST_COMMENT_PAGE_FILE)) {
+		const contents: string = fs.readFileSync(LAST_COMMENT_PAGE_FILE).toString();
+		lastPage = +contents;
+	}
+	// Determine how many pages of questions there are because AnswerHub doesn't let you sort by oldest comments first
+	const pageCount = (await answerHub.getComments(0, "newest")).pageCount;
+	for (let page = pageCount - lastPage; page >= 0; page--) {
+		await downloadCommentPage(page);
+		fs.writeFileSync(LAST_COMMENT_PAGE_FILE, page);
 	}
 };
 
 
-const downloadPage = async (page: number) => {
-	console.log("Downloading page " + page);
+const downloadQuestionPage = async (page: number) => {
+	console.log("Downloading question page " + page);
 	const questionList = await answerHub.getQuestions(page, "newest");
 	for (const question of questionList.list) {
 		try {
@@ -52,4 +67,28 @@ const downloadPage = async (page: number) => {
 	}
 };
 
-downloadQuestions();
+const downloadCommentPage = async (page: number) => {
+	console.log("Downloading comment page " + page);
+	const commentList = await answerHub.getComments(page, "newest");
+	for (const comment of commentList.list) {
+		try {
+			console.log(`Downloading comment ${comment.id}...`);
+			const contents = {
+				id: comment.id,
+				body: answerHub.formatQuestionBody(comment.body)
+			};
+			fs.writeFileSync(`${__dirname}/../data/comments/${comment.id}.json`, JSON.stringify(contents));
+		} catch (ex) {
+			console.error(`An error occurred while downloading comment ${comment.id}: ${ex}`);
+		}
+	}
+};
+
+Promise.all([
+	downloadQuestions(),
+	downloadComments()
+]).then(() => {
+	console.log("Done!");
+}, (ex) => {
+	console.error(ex);
+});
