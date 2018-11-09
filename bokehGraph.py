9c0e692db35310a03e92fdfac12bb2548b2976d3
singerm0 <35753471+singerm0@users.noreply.github.com>
2018-11-09 13:45:22 -0500

    Add files via upload
    graphing tool for bokeh, make sure your have the embedder in your python path and to change the location for the question-answers.json

---
+++ bokehGraph.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 20:36:52 2018
+
+@author: Matthew
+ 
+creates multicolored hover over movable graph with bokeh,
+make sure to change the syspath for autoguru and the model location on your run
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.manifold import TSNE
+import bokeh.plotting as bk
+from bokeh.models import HoverTool
+from copy import deepcopy
+import json
+import random
+from itertools import compress
+import collections
+
+
+import sys
+# manually adds auto guru to my python path
+sys.path.append(r'E:\hackathon\autoguru\question-answering')
+from questionanswering.embeddings import Embedder
+#change this for your program
+
+def main():
+    model = 'E:\\hackathon\\answer\\embedder.npz'
+    # eembedder model location, change for yourprogram
+    embedder = Embedder.load(model)
+    
+    with open('E://hackathon//question-answers.json') as f:
+        pracDic = collections.OrderedDict(json.load(f))
+    # load json dict    
+    embedded = np.zeros((len(pracDic),300))
+    ind = 0
+    question = []
+    answers = []
+    # initialize data structures
+    for x in pracDic.items():
+        question.append(x[0])
+        answers.append(x[1])
+        embedded[ind,:] = embedder.embed(x[0])
+        ind += 1
+    #construct structures from dictionary
+    
+    embedded = pd.DataFrame(embedded) # make embedded a dataframe
+    
+    # not needed, nothing should be null
+    #pracDic = dict(compress(list(pracDic.items()),list(~embedded.isnull().any(axis=1))))
+    #embedded = embedded.loc[~embedded.isnull().any(axis=1),:]
+    
+    tsne = TSNE(n_components =2,verbose=0,perplexity=12,n_iter=10000, early_exaggeration =5)
+    tsne_results = np.array(tsne.fit_transform(embedded))
+    # creates tsne model and fits our data to it
+    finalQuest = pd.DataFrame({'quest':question,'ans':answers,'origVects':np.array(embedded).tolist(),
+                               'vectX':tsne_results[:,0],'vectY':tsne_results[:,1]})
+    df = deepcopy(finalQuest)
+    # this isnt needed i didnt want to mess with finalQuest while debugging
+        
+    def splitFrame(df):
+        ''' creates a list of dataframes of same size, one dataframe is created for each question
+        each dataframe has null rows for every question besides the i'th quesiton'''
+        count = 1
+        dfList = []
+        tempDf = pd.DataFrame([])
+        tempDf['quest'] = df['quest']
+        tempDf['x'] = df['vectX']
+        tempDf['y'] = df['vectY']
+        tempDf['ans'] = df['ans']
+        tempDf['count'] = pd.DataFrame([0]*int(len(tempDf)))
+        ansSet = list(set(df['ans']))
+        for x in range(len(set(df['ans']))):
+            storedBool = df['ans'] == ansSet[x]
+            tempDf['count'] = tempDf['count'] + storedBool*count
+            count += 1
+        
+        for x in range(len(set(df['ans']))):
+            manipDf = deepcopy(tempDf)
+            manipDf.loc[manipDf['count']!= x+1,:] = np.NaN
+            dfList.append(manipDf)
+        return(dfList)
+    splits = splitFrame(df)
+    
+    bk.output_file("toolbar.html")
+    #sets the output url    
+    TOOLTIPS = ''' 
+        <div>
+            <div> 
+                <span style="font-size: 17px; font-weight: bold;">Question:</span> 
+            </div>
+            <div> 
+                <span style="font-size: 17px;">@quest{safe}</span> 
+            </div>
+            <div> 
+                <span style="font-size: 17px; font-weight: bold;">Answer:</span> 
+            </div>
+            <div> 
+                <span style="font-size: 17px;">@ans{safe}</span> 
+            </div>
+        </div>'''
+    #creates the hover over text
+    
+    p = bk.figure(plot_width=850, plot_height=700,title='Questions and Answers')
+    p.title.text_color = 'black'
+    p.title.text_font = 'helvetica'
+    p.title.text_font_size = '24pt'
+    p.background_fill_color = '#f4f3ef'
+    p.xaxis.visible =False
+    p.yaxis.visible = False
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_color = None
+    #sets details for graph
+    
+    
+    color = []
+    for x in range(1000):
+        color.append("#%06x" % random.randint(0, 0xFFFFFF))
+    colors = list(set(color))
+    #generates a list of colors
+    
+    # manually selecting colors, this is just for hte demo, comment out this line if using more
+    colors = ['firebrick','royalblue','peru','teal','seagreen','darkmagenta','gold']
+    
+    for x in range(len(splits)):
+        r = p.scatter('x','y', size=10,source=bk.ColumnDataSource(splits[x]),color=colors[x])
+        p.add_tools(HoverTool(renderers=[r], tooltips=TOOLTIPS))
+        #plots all of the different colored points with their respective hover text
+        
+    bk.show(p)
+    #shows the plot
+if __name__ == '__main__':
+    main()
